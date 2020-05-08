a24d56813fc8bece3d9f740753386682291966fd
Eero Sova <t7soee00@students.oamk.fi>
2020-05-08 20:25:20 +0300

    Added working adapter for recyclerview

--- app/src/main/java/com/labra/mytodotehtava/MainActivity.java
+++ app/src/main/java/com/labra/mytodotehtava/MainActivity.java
@@ -1,45 +1,145 @@
 package com.labra.mytodotehtava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity  {
+
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-    String[] myDataset = {
-            "Yksi",
-            "Kaksi",
-            "Kolme",
-            "neljä",
-            "viisi"
-    };
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
+    private EditText nimi, selostus;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
+        nimi = findViewById(R.id.nimiKentta);
+        selostus = findViewById(R.id.selostusKentta);
+
+
+        recyclerView = findViewById(R.id.list);
+
+
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
+        fetch();
 
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
 
-        mAdapter = new MyAdapter(myDataset);
-        recyclerView.setAdapter(mAdapter);
+
+
+
+        Button lisaaTehtava = findViewById(R.id.btn);
+        lisaaTehtava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("tehtavat").push();
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", databaseReference.getKey());
+                map.put("title", nimi.getText().toString());
+                map.put("desc", selostus.getText().toString());
+                databaseReference.setValue(map);
+            }
+
+        });
+
+
+    }
+    private void fetch() {
+        Query query = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("tehtavat");
+
+        FirebaseRecyclerOptions<Model> options =
+                new FirebaseRecyclerOptions.Builder<Model>()
+                        .setQuery(query, new SnapshotParser<Model>() {
+                            @NonNull
+                            @Override
+                            public Model parseSnapshot(@NonNull DataSnapshot snapshot) {
+                                return new Model(snapshot.child("id").getValue().toString(),
+                                        snapshot.child("title").getValue().toString(),
+                                        snapshot.child("desc").getValue().toString());
+                            }
+                        })
+                        .build();
+
+        adapter = new FirebaseRecyclerAdapter<Model, ViewHolder>(options) {
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_item, parent, false);
+
+                return new ViewHolder(view);
+            }
+
+
+            @Override
+            protected void onBindViewHolder(ViewHolder holder, final int position, Model model) {
+                holder.setTxtTitle(model.getmTitle());
+                // holder.setTxtDesc(model.getmDesc());
+
+                holder.root.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        };
+        recyclerView.setAdapter(adapter);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 
-   // @Override
-  //  public void onNoteClick(int position) {
-  //      Intent intent = new Intent(this, );
-   //     startActivity(intent);
-  //  }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+
+
 }
+
+
+
+
+
